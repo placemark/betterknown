f9014d01263987e05af8b03cf35ca6c5d12e45a6
Severino <severin.opel@hotmail.com>
2025-04-29 14:24:02 +0200

    Made wktToGeoJSON case insensitive

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,215 +1,236 @@
 import { WktUserOptions, geoJSONToWkt, wktToGeoJSON } from "./index";
-import { test, describe, it, expect } from "vitest";
+import { describe, it, expect } from "vitest";
 import { Geometry } from "geojson";
 import proj4 from "proj4";
-import { WKT_GEOMETRY_TYPES } from "../constants";
+import { EMPTY, WKT_GEOMETRY_TYPES, WKTGeometryType } from "../constants";
 
-function reversible(
-  wkt1: string,
-  geometry1: Geometry,
-  userOptions: WktUserOptions = {
-    emptyAsNull: true,
-  }
-) {
-  const geometry = wktToGeoJSON(wkt1, userOptions);
-  expect(geometry).not.toBeNull();
-  const wkt = geoJSONToWkt(geometry!);
-  expect(geometry).toEqual(geometry1);
-  expect(wkt).toEqual(wkt1);
+const WKT_GEOMETRY_TYPES_MAP = WKT_GEOMETRY_TYPES.reduce((map, type) => {
+  map[type] = type;
+  return map;
+}, {} as Record<WKTGeometryType, string>);
+
+const dictionary = {
+  Srid: "SRID",
+  Z: "Z",
+  Empty: "EMPTY",
 }
+type Dictionary = typeof dictionary;
 
-describe("parsing and stringifying", () => {
-  it("overflow resistance", () => {
-    const bigString = `MULTIPOLYGON (((${Array.from(
-      { length: 1000 },
-      (_, i) => `${i}, ${i}`
-    ).join(",")})))`;
-    expect(wktToGeoJSON(bigString)).toHaveProperty("type", "MultiPolygon");
-  });
-  it("default as null on empty", () => {
-    for (const name of WKT_GEOMETRY_TYPES) {
-      expect(wktToGeoJSON(`${name} EMPTY`)).toBeNull();
-      expect(
-        wktToGeoJSON(`${name} EMPTY`, {
-          emptyAsNull: false,
-        })
-      ).toBeTruthy();
+export function testCases(wktGeometriesMap: Record<WKTGeometryType, string>, dictionary: Dictionary) {
+  function reversible(
+    wkt1: string,
+    geometry1: Geometry,
+    userOptions: WktUserOptions = {
+      emptyAsNull: true,
     }
-  });
-  it("point", () => {
-    reversible(
-      "POINT EMPTY",
-      {
-        type: "Point",
-        coordinates: [],
-      },
-      {
-        emptyAsNull: false,
-      }
-    );
-    reversible("POINT (1 20)", {
-      type: "Point",
-      coordinates: [1, 20],
+  ) {
+    const geometry = wktToGeoJSON(wkt1, userOptions);
+    expect(geometry).not.toBeNull();
+    const wkt = geoJSONToWkt(geometry!);
+    expect(geometry).toEqual(geometry1);
+    /**
+    * The passed WKT is case insensitive,
+    * but the output of geoJSONToWkt is upper case.
+    * Therefore we cast the returned WKT to upper case.
+    */
+    expect(wkt).toEqual(wkt1.toUpperCase());
+  }
+
+  describe("parsing and stringifying", () => {
+    it("overflow resistance", () => {
+      const bigString = `${wktGeometriesMap.MultiPolygon} (((${Array.from(
+        { length: 1000 },
+        (_, i) => `${i}, ${i}`
+      ).join(",")})))`;
+      expect(wktToGeoJSON(bigString)).toHaveProperty("type", "MultiPolygon");
     });
-    reversible("POINT (-1.5 20.5)", {
-      type: "Point",
-      coordinates: [-1.5, 20.5],
-    });
-  });
-  it("geometrycollection", () => {
-    reversible(
-      "GEOMETRYCOLLECTION EMPTY",
-      {
-        type: "GeometryCollection",
-        geometries: [],
-      },
-      {
-        emptyAsNull: false,
+    it("default as null on empty", () => {
+      for (const name of Object.values(wktGeometriesMap)) {
+        expect(wktToGeoJSON(`${name} ${dictionary.Empty}`)).toBeNull();
+        expect(
+          wktToGeoJSON(`${name} ${dictionary.Empty}`, {
+            emptyAsNull: false,
+          })
+        ).toBeTruthy();
       }
-    );
-    reversible("GEOMETRYCOLLECTION(POINT (-1.5 20.5))", {
-      type: "GeometryCollection",
-      geometries: [
+    });
+    it("point", () => {
+      reversible(
+        `${wktGeometriesMap.Point} ${dictionary.Empty}`,
         {
           type: "Point",
-          coordinates: [-1.5, 20.5],
+          coordinates: [],
         },
-      ],
+        {
+          emptyAsNull: false,
+        }
+      );
+      reversible(`${wktGeometriesMap.Point} (1 20)`, {
+        type: "Point",
+        coordinates: [1, 20],
+      });
+      reversible(`${wktGeometriesMap.Point} (-1.5 20.5)`, {
+        type: "Point",
+        coordinates: [-1.5, 20.5],
+      });
     });
-  });
-  it("multipoint", () => {
-    reversible("POINT Z (-1.5 20.5 5)", {
-      type: "Point",
-      coordinates: [-1.5, 20.5, 5],
+    it("geometrycollection", () => {
+      reversible(
+        `${wktGeometriesMap.GeometryCollection} ${dictionary.Empty}`,
+        {
+          type: "GeometryCollection",
+          geometries: [],
+        },
+        {
+          emptyAsNull: false,
+        }
+      );
+      reversible(`${wktGeometriesMap.GeometryCollection}(${wktGeometriesMap.Point} (-1.5 20.5))`, {
+        type: "GeometryCollection",
+        geometries: [
+          {
+            type: "Point",
+            coordinates: [-1.5, 20.5],
+          },
+        ],
+      });
     });
-    reversible(
-      "MULTIPOINT EMPTY",
-      {
+    it("multipoint", () => {
+      reversible(`${wktGeometriesMap.Point} ${dictionary.Z} (-1.5 20.5 5)`, {
+        type: "Point",
+        coordinates: [-1.5, 20.5, 5],
+      });
+      reversible(
+        `${wktGeometriesMap.MultiPoint} ${dictionary.Empty}`,
+        {
+          type: "MultiPoint",
+          coordinates: [],
+        },
+        {
+          emptyAsNull: false,
+        }
+      );
+      reversible(`${wktGeometriesMap.MultiPoint} (1 2,3 4)`, {
         type: "MultiPoint",
-        coordinates: [],
-      },
-      {
-        emptyAsNull: false,
-      }
-    );
-    reversible("MULTIPOINT (1 2,3 4)", {
-      type: "MultiPoint",
-      coordinates: [
-        [1, 2],
-        [3, 4],
-      ],
-    });
-  });
-  it("linestring", () => {
-    reversible("LINESTRING (1 2,3 4)", {
-      type: "LineString",
-      coordinates: [
-        [1, 2],
-        [3, 4],
-      ],
-    });
-    reversible(
-      "LINESTRING EMPTY",
-      {
-        type: "LineString",
-        coordinates: [],
-      },
-      {
-        emptyAsNull: false,
-      }
-    );
-  });
-  it("multilinestring", () => {
-    reversible("MULTILINESTRING ((1 2,3 4,1 2))", {
-      type: "MultiLineString",
-      coordinates: [
-        [
+        coordinates: [
           [1, 2],
           [3, 4],
-          [1, 2],
         ],
-      ],
+      });
     });
-    reversible(
-      "MULTILINESTRING EMPTY",
-      {
+    it("linestring", () => {
+      reversible(`${wktGeometriesMap.LineString} (1 2,3 4)`, {
+        type: "LineString",
+        coordinates: [
+          [1, 2],
+          [3, 4],
+        ],
+      });
+      reversible(
+        `${wktGeometriesMap.LineString} ${dictionary.Empty}`,
+        {
+          type: "LineString",
+          coordinates: [],
+        },
+        {
+          emptyAsNull: false,
+        }
+      );
+    });
+    it("multilinestring", () => {
+      reversible(`${wktGeometriesMap.MultiLineString} ((1 2,3 4,1 2))`, {
         type: "MultiLineString",
-        coordinates: [],
-      },
-      {
-        emptyAsNull: false,
-      }
-    );
-  });
-  it("multipolygon", () => {
-    reversible("MULTIPOLYGON (((1 2,3 4,1 2)))", {
-      type: "MultiPolygon",
-      coordinates: [
-        [
+        coordinates: [
           [
             [1, 2],
             [3, 4],
             [1, 2],
           ],
         ],
-      ],
+      });
+      reversible(
+        `${wktGeometriesMap.MultiLineString} ${dictionary.Empty}`,
+        {
+          type: "MultiLineString",
+          coordinates: [],
+        },
+        {
+          emptyAsNull: false,
+        }
+      );
     });
-    reversible(
-      "MULTIPOLYGON EMPTY",
-      {
+    it("multipolygon", () => {
+      reversible(`${wktGeometriesMap.MultiPolygon} (((1 2,3 4,1 2)))`, {
         type: "MultiPolygon",
-        coordinates: [],
-      },
-      {
-        emptyAsNull: false,
-      }
-    );
-  });
-  it("polygon", () => {
-    reversible("POLYGON Z ((1 2 10,3 4 10,1 2 10))", {
-      type: "Polygon",
-      coordinates: [
-        [
-          [1, 2, 10],
-          [3, 4, 10],
-          [1, 2, 10],
+        coordinates: [
+          [
+            [
+              [1, 2],
+              [3, 4],
+              [1, 2],
+            ],
+          ],
         ],
-      ],
+      });
+      reversible(
+        `${wktGeometriesMap.MultiPolygon} ${dictionary.Empty}`,
+        {
+          type: "MultiPolygon",
+          coordinates: [],
+        },
+        {
+          emptyAsNull: false,
+        }
+      );
     });
-    reversible("POLYGON ((1 2,3 4,1 2))", {
-      type: "Polygon",
-      coordinates: [
-        [
-          [1, 2],
-          [3, 4],
-          [1, 2],
-        ],
-      ],
-    });
-    reversible(
-      "POLYGON EMPTY",
-      {
+    it("polygon", () => {
+      reversible(`${wktGeometriesMap.Polygon} ${dictionary.Z} ((1 2 10,3 4 10,1 2 10))`, {
         type: "Polygon",
-        coordinates: [],
-      },
-      {
-        emptyAsNull: false,
-      }
-    );
-  });
+        coordinates: [
+          [
+            [1, 2, 10],
+            [3, 4, 10],
+            [1, 2, 10],
+          ],
+        ],
+      });
+      reversible(`${wktGeometriesMap.Polygon} ((1 2,3 4,1 2))`, {
+        type: "Polygon",
+        coordinates: [
+          [
+            [1, 2],
+            [3, 4],
+            [1, 2],
+          ],
+        ],
+      });
+      reversible(
+        `${wktGeometriesMap.Polygon} ${dictionary.Empty}`,
+        {
+          type: "Polygon",
+          coordinates: [],
+        },
+        {
+          emptyAsNull: false,
+        }
+      );
+    });
 
-  it("ewkt", () => {
-    expect(wktToGeoJSON(`SRID=4326;POINT(-44.3 60.1)`)).toEqual({
-      coordinates: [-44.3, 60.1],
-      type: "Point",
-    });
-    expect(
-      wktToGeoJSON(`SRID=3857;POINT(-400004.3 60000.1)`, {
-        proj: proj4,
-      })
-    ).toEqual({
-      coordinates: [-3.5932997640353026, 0.5389821193537617],
-      type: "Point",
+    it("ewkt", () => {
+      expect(wktToGeoJSON(`${dictionary.Srid}=4326;${wktGeometriesMap.Point}(-44.3 60.1)`)).toEqual({
+        coordinates: [-44.3, 60.1],
+        type: "Point",
+      });
+      expect(
+        wktToGeoJSON(`${dictionary.Srid}=3857;${wktGeometriesMap.Point}(-400004.3 60000.1)`, {
+          proj: proj4,
+        })
+      ).toEqual({
+        coordinates: [-3.5932997640353026, 0.5389821193537617],
+        type: "Point",
+      });
     });
   });
-});
+};
+
+testCases(WKT_GEOMETRY_TYPES_MAP, dictionary);
---
+++ test/lowercase.test.ts
@@ -0,0 +1,15 @@
+import { WKT_GEOMETRY_TYPES, EMPTY } from "../constants";
+import type { WKTGeometryType } from "../constants";
+import { testCases } from "./index.test";
+
+const LowerCaseMap: Record<WKTGeometryType, string> = WKT_GEOMETRY_TYPES.reduce((map, type) => {
+  const lowerCaseType = type.toLowerCase();
+  map[type] = lowerCaseType;
+  return map;
+}, {} as Record<WKTGeometryType, string>);
+
+testCases(LowerCaseMap, {
+  Srid: "srid",
+  Z: "z",
+  Empty: "empty",
+});
